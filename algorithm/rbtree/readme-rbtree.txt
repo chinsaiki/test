f7d0c371996e3fd212dde9e49d5f839c0d520bd2
root <root@localhost.localdomain>
2020-11-24 10:59:32 +0800

    jemalloc前缀接口je_xxx

---
+++ algorithm/rbtree/readme-rbtree.txt
@@ -0,0 +1,6 @@
+红黑树
+
+---------------------------------
+*** test/jemalloc/src-code/rb/h
+*** test/algorithm/rbtree
+*** test/algorithm/rbtree/jemalloc
---
+++ jemalloc/src-code/test_1_rbtree.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "rb.h"
+
+rb_tree(int);
+
+
+int main()
+{
+    rb_tree(int) itree;
+
+    rb_new(int, 0, &itree);
+
+    
+}
