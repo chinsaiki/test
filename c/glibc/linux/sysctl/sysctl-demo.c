4befd7fa38dd3f43931e34243ce789c260990b45
root <root@localhost.localdomain>
2020-12-01 13:05:20 +0800

    gtest: Google Test Tools

--- c/glibc/linux/sysctl/sysctl-demo.c
+++ c/glibc/linux/sysctl/sysctl-demo.c
@@ -6,6 +6,13 @@
 #include <stdlib.h>
 #include <linux/sysctl.h>
 
+//Glibc  does  not  provide  a wrapper for this system call; call it using syscall(2).  Or rather...  don't
+//call it: use of this system call has long been discouraged, and it is so unloved that  it  is  likely  to
+//disappear in a future kernel version.  Since Linux 2.6.24, uses of this system call result in warnings in
+//the kernel log.  Remove it from your programs now; use the /proc/sys interface instead.
+//
+//This system call is available only if the kernel was configured with the CONFIG_SYSCTL_SYSCALL option.
+
 int _sysctl(struct __sysctl_args *args );
 
 #define OSNAMESZ 100
@@ -28,7 +35,7 @@ main(void)
 
    if (syscall(SYS__sysctl, &args) == -1) {
        perror("_sysctl");
-       //_sysctl: Function not implemented 函数未执行
+       //_sysctl: Function not implemented 函数未执行,
        exit(EXIT_FAILURE);
    }
    printf("This machine is running %*s\n", osnamelth, osname);
