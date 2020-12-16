e3963740de4189fed035c5f62ce5ae6c02af52a4
root <root@localhost.localdomain>
2020-12-16 13:03:30 +0800

    Some Study

---
+++ gdb/study/watch/test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <malloc.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    int a=0;
+    int i;
+    for(i=0;i<10;i++) {
+        a += i;
+    }
+
+    return 0;
+}
