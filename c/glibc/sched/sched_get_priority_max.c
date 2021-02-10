490bc83c9b59b97f2b82b21eb1183022fd6c5bf8
rtoax <rongtao@localhost.localdomain>
2021-02-10 15:27:18 +0800

    sched.h

--- c/glibc/sched/sched_get_priority_max.c
+++ c/glibc/sched/sched_get_priority_max.c
@@ -27,3 +27,22 @@
  *	EINVAL policy does not identify an existing scheduling policy.
  */
 #include <sched.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sched.h>
+
+int main()
+{
+  printf("Valid priority range for SCHED_OTHER: %d - %d\n",
+         sched_get_priority_min(SCHED_OTHER),
+         sched_get_priority_max(SCHED_OTHER));
+
+  printf("Valid priority range for SCHED_FIFO: %d - %d\n",
+         sched_get_priority_min(SCHED_FIFO),
+         sched_get_priority_max(SCHED_FIFO));
+
+  printf("Valid priority range for SCHED_RR: %d - %d\n",
+         sched_get_priority_min(SCHED_RR),
+         sched_get_priority_max(SCHED_RR));
+}
+
