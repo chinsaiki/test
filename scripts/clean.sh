accd2690f010da5abb89f61aabf174601711ea82
rtoax <rongtao@localhost.localdomain>
2021-02-24 14:56:13 +0800

    删除无用文件

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 # 荣涛 
 
-
 function cleanall {
 	filename=("*.o" "*.d" "*.gch" "*~"  "*.out" "core.*" "vgcore.*" "*.exe" "*.so" "*.a")
 	for name in ${filename[@]}; do
@@ -20,3 +19,26 @@ function clean {
 	return 0
 }
 
+function cleandir {
+	if [ $# -lt 1 ]; then
+		echo "Usage: $0 [directory list]"
+		return 0
+	fi
+	QA2 "Are you sure to remove \"$*\" ? "
+	if [ $? != 0 ]; then
+		return 0
+	fi
+	for dir in $*
+	do
+		if [ -d $dir ]; then
+			cd $dir
+			# 删除一般文件
+			rm -rf *
+			# 删除隐藏文件
+			rm -rf `find -name "\.[0-9a-zA-Z]*"`
+			echo Clean directory $dir for further. `date` > CLEANDIR
+			cd ..
+		fi
+	done
+	return 0
+}
